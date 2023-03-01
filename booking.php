8fa0e4115a050000a3609116c1a2142fe85aeb55
vilavr <vilavr@ttu.ee>
2023-03-01 18:05:57 +0000

    booking form data to order confirmation php (refactored)

--- booking.php
+++ booking.php
@@ -62,28 +62,7 @@
         <div class="order-form">
             <div class="user-info-disclaimer">
                 <p>Enter your info (or confirm that it is correct)</p>
-                <?php
-                    session_start();
-
-                    if (isset($_POST['submit-form'])) {
-                        // Capture form data
-                        $name = $_POST['name'];
-                        $email = $_POST['email'];
-                        $surname = $_POST['surname'];
-                        $phone = $_POST['phone'];
-                        $address = $_POST['address'];
-
-                        // Store form data in session variable
-                        $_SESSION['name'] = $name;
-                        $_SESSION['email'] = $email;
-                        $_SESSION['surname'] = $surname;
-                        $_SESSION['phone'] = $phone;
-                        $_SESSION['address'] = $address;
-
-                        // Display success message
-                        // echo "<p>Form submitted successfully!</p>";
-                    }
-                ?>
+                
 
                 <form action="order_confirmation.php" method="post" class="form-rows">
                     <div class="input-lines">
--- order_confirmation.php
+++ order_confirmation.php
@@ -1,3 +1,20 @@
+<?php
+    session_start();
+    if (isset($_POST['submit-form'])) {
+        // Capture form data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $surname = $_POST['surname'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        // Store form data in session variable
+        $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['surname'] = $surname;
+        $_SESSION['phone'] = $phone;
+        $_SESSION['address'] = $address;
+    }
+?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -50,15 +67,6 @@
         <div class="personal-data-header">
             <div class="pdata-header"><p>Personal info</p></div>
             <div class="pdata">
-                <?php
-                    session_start();
-                    $name = $_SESSION['name'];
-                    $email = $_SESSION['email'];
-                    $phone = $_SESSION['phone'];
-                    $address = $_SESSION['address'];
-                    $surname = $_SESSION['surname'];
-                ?>
-
                 <!-- Display form data on page -->
                 <p><?php echo $name; ?>, <?php echo $surname; ?></p>
                 <p><?php echo $phone; ?></p>
