0f728064bb42c879bfbfc0d27839a6089579a932
Ksenija Okuneva <ksokun@ttu.ee>
2023-03-19 21:59:45 +0200

    calendar fixes, weight of items block

--- js/bulk-waste-item.js
+++ js/bulk-waste-item.js
@@ -7,31 +7,50 @@ const addButton = document.querySelector('.add-btn');
 
 // Add a click event listener to the add button
 addButton.addEventListener('click', () => {
-    // Create a new form line
-    const newFormLine = document.createElement('div');
-    newFormLine.classList.add('form-line');
+  // Create a new form line
+  const newFormLine = document.createElement('div');
+  newFormLine.classList.add('form-line');
 
-    // Add the HTML for the new form line
-    newFormLine.innerHTML = `
-        <input type="text" name="field[]" placeholder="Field" />
-        <select name="selector[]">
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-        </select>
-        <button class="remove-btn">&minus;</button>
-    `;
+  // Determine the number of existing form lines
+  const numFormLines = formContainer.querySelectorAll('.form-line').length;
 
-    // Add a click event listener to the remove button
-    const removeButton = newFormLine.querySelector('.remove-btn');
-    removeButton.addEventListener('click', () => {
-        newFormLine.remove();
-    });
+  // Add the HTML for the new form line
+  newFormLine.innerHTML = `
+    <label for="selector${numFormLines + 1}" class="item">Item ${numFormLines + 1}</label>
+    <select name="selector[]" id="selector${numFormLines + 1}">
+      <option value="option1">less than 5kg</option>
+      <option value="option2">5 - 10 kg</option>
+      <option value="option3">10 - 50 kg</option>
+      <option value="option3">50 - 100 kg</option>
+      <option value="option3">100 - 500 kg</option>
+    </select>
+    <button class="remove-btn">&minus;</button>
+  `;
 
-    // Add the new form line to the form container
-    formContainer.insertBefore(newFormLine, addButton.parentElement);
+  // Add a click event listener to the remove button
+  const removeButton = newFormLine.querySelector('.remove-btn');
+  removeButton.addEventListener('click', () => {
+    newFormLine.remove();
+  });
+
+  // Add the new form line to the form container
+  formContainer.insertBefore(newFormLine, addButton.parentElement);
+
+  // Update the labels for all form lines
+  updateFormLineLabels();
 });
 
+// Update the labels for all form lines
+function updateFormLineLabels() {
+  const formLines = formContainer.querySelectorAll('.form-line');
+  formLines.forEach((formLine, index) => {
+    const label = formLine.querySelector('label');
+    label.textContent = `Item ${index + 1}`;
+    const select = formLine.querySelector('select');
+    select.id = `selector${index + 1}`;
+    label.setAttribute('for', `selector${index + 1}`);
+  });
+}
 
 // Fields appear only when bulk waste removal is checked
 // Hide the form container by default
